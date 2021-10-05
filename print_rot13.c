bc01f7381994f2cbbc4a503d64cd5b3fc997787e
birhanus <birhanushimles716@gmail.com>
2021-10-05 07:18:04 -0700

    function to print specifiers of an encoder

--- print_rot13.c
+++ print_rot13.c
@@ -15,3 +15,25 @@ int print_R(va_list R)
 	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+	str = va_arg(R, char *);
+	if (str == NULL)
+		str = "(ahyy)";
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[i])
+			{
+				_putchar(out[j]);
+				count++;
+				break;
+			}
+		}
+		if (!in[j])
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
